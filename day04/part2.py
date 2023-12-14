8f9361d786bd50a5987c7d083a0ffc2a3b64a065
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-12-14 09:51:42 -0500

    Day 4 Part 2

---
+++ day04/part2.py
@@ -0,0 +1,47 @@
+import os
+import math
+
+
+def main():
+    input = os.path.join(os.path.dirname(__file__), "input.txt")
+    num_lines = len(open(input, "r").readlines())
+
+    # Store the number of copies for each card at index card_number - 1
+    copies = [1] * num_lines
+
+    f = open(input, "r")
+    card_number = 0
+
+    for card in f:
+        card_number += 1
+        card, numbers = card.split(": ")
+        winning_nums, my_nums = numbers.split("|")
+        curr_number = 0
+        winning_nums_list = []
+
+        for x in winning_nums:
+            if not x.isdigit():
+                winning_nums_list.append(curr_number)
+                curr_number = 0
+            else:
+                curr_number = (curr_number * 10) + int(x)
+
+        curr_number = 0
+        count = 0
+
+        for y in my_nums:
+            if not y.isdigit():
+                if curr_number != 0 and curr_number in winning_nums_list:
+                    count += 1
+                    copies[card_number - 1 + count] += copies[card_number - 1]
+                curr_number = 0
+            else:
+                curr_number = (curr_number * 10) + int(y)
+
+        res = sum(copies)
+
+    print(res)
+    return res
+
+
+main()
